e8378c031cdee48750742fe88cc7d3647de083cd
Pradyumna Kumar Naik <92319505+Pradyumna-1@users.noreply.github.com>
2025-05-05 10:12:28 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',       // Your MySQL host
-  user: 'root',            // Your MySQL username
-  password: 'root',            // Your MySQL password (empty if none)
+  host: 'localhost',       //  MySQL host
+  user: 'root',            //  MySQL username
+  password: 'root',            //  MySQL password
   database: 'employee_management', // Database name
   waitForConnections: true,
   connectionLimit: 10,
